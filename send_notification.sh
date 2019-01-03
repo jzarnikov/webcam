1a3b09f7ec4e55fc979f9601e00b8216e227640b
Jan Zarnikov <jan.zarnikov@gmail.com>
2019-01-03 16:31:16 +0100

    Create send_notification.sh

---
+++ send_notification.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+curl -X POST -s \
+  --form-string "app_key=$PUSHED_APP_KEY" \
+  --form-string "app_secret=$PUSHED_APP_SECRET" \
+  --form-string "target_type=app" \
+  --form-string "content=$1" \
+  https://api.pushed.co/1/push
