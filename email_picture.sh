30ff50b8dc6d603e73e70d51883d311f05e33b9d
Jan Zarnikov <jan.zarnikov@gmail.com>
2019-02-10 11:45:55 +0100

    disabling

--- email_picture.sh
+++ email_picture.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-timeout --signal=KILL 15s mpack -s motion-detected $1 $EMAIL_RECIPIENT
-exit_status=$?
-if [[ $exit_status -eq 124 ]]; then
-    echo "Send timed out" | mail -s "motion-detected - ERROR" $EMAIL_RECIPIENT
-    mpack -s motion-detected $1 $EMAIL_RECIPIENT
+if [ ! -f disabled ]; then
+    timeout --signal=KILL 15s mpack -s motion-detected $1 $EMAIL_RECIPIENT
+    exit_status=$?
+    if [[ $exit_status -eq 124 ]]; then
+        echo "Send timed out" | mail -s "motion-detected - ERROR" $EMAIL_RECIPIENT
+        mpack -s motion-detected $1 $EMAIL_RECIPIENT
+    fi
 fi
+
--- send_notification.sh
+++ send_notification.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-timeout --signal=KILL $2 curl -X POST -s --form-string "app_key=$PUSHED_APP_KEY" --form-string "app_secret=$PUSHED_APP_SECRET" --form-string "target_type=app" --form-string "content=$1" https://api.pushed.co/1/push
+if [ ! -f disabled ]; then
+    timeout --signal=KILL $2 curl -X POST -s --form-string "app_key=$PUSHED_APP_KEY" --form-string "app_secret=$PUSHED_APP_SECRET" --form-string "target_type=app" --form-string "content=$1" https://api.pushed.co/1/push
+fi
