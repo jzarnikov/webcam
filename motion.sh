dcbe1e3814f00de97dfe80b3d7600335524e9939
Jan Zarnikov <jan.zarnikov@gmail.com>
2019-03-03 20:17:11 +0100

    mounting

--- motion.sh
+++ motion.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+if mountpoint -q /home/pi/drive
+then
+   echo "drive already mounted"
+else
+   echo "mounting drive"
+   /home/pi/.opam/system/bin/google-drive-ocamlfuse /home/pi/drive
+fi
+
 motion 2>&1 | tee -a motion.log
